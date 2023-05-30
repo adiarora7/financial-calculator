684b2739d0d6f91ed7bb42598250efaad2b0c69e
adiarora7 <96757084+adiarora7@users.noreply.github.com>
2023-05-30 15:58:04 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,8 @@ export default function Home() {
       <div className='main-text'>
         <p>The CAGR calculator is a useful tool for anyone who wants to estimate the gain from an investment. This application bases its calculations on the Compound Annual Growth Rate formula (CAGR formula). If you know how to calculate the growth rate, you can determine the profit of your investment over a particular period. 
          <br/><br/>
-         To save an investments CAGR value, a name must be provided for the Investment Name.
+         *To save an investments CAGR value, a name must be provided for the Investment Name.
+         <br/>
          </p>
       </div>
     </main>
